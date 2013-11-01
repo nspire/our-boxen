b1b473fb45136b0be3ba6f13bc2324b2286d7295
Alykhan Kanji <github@alykhan.com>
2013-10-31 21:48:07 -0400

    Set defaults for General prefs

--- modules/people/manifests/alykhank.pp
+++ modules/people/manifests/alykhank.pp
@@ -4,4 +4,19 @@ class people::alykhank {
   include spectacle
 
   include osx::dock::autohide
+
+  boxen::osx_defaults { 'Ask to keep changes when closing documents':
+    ensure     => present,
+    domain     => 'NSGlobalDomain',
+    key        => 'NSCloseAlwaysConfirmsChanges',
+    value      => true,
+    user       => $::boxen_user;
+  }
+  boxen::osx_defaults { 'Close windows when quitting an application':
+    ensure     => present,
+    domain     => 'NSGlobalDomain',
+    key        => 'NSQuitAlwaysKeepsWindows',
+    value      => true,
+    user       => $::boxen_user;
+  }
 }
